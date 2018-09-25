ee2077a0e9fe208be8440da65f63b7af7e6e3657
Simonluca Landi <simonluca.landi@gmail.com>
2018-09-25 06:47:42 +0000

    temporary

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,10 +6,30 @@ if [[ "$1" == "bitcoin-cli" || "$1" == "bitcoin-tx" || "$1" == "bitcoind" || "$1
 
     if [[ ! -s "$BITCOIN_DATA/bitcoin.conf" ]]; then
 	cat <<-EOF > "$BITCOIN_DATA/bitcoin.conf"
-	printtoconsole=1
-	rpcallowip=::/0
-	rpcpassword=${BITCOIN_RPC_PASSWORD:-password}
-	rpcuser=${BITCOIN_RPC_USER:-bitcoin}
+# Bitcoind options
+server=1
+txindex=0
+disablewallet=1
+prune=550
+
+# Connection settings
+
+onlynet=ipv4
+zmqpubrawblock=tcp://0.0.0.0:29000
+zmqpubrawtx=tcp://0.0.0.0:29001
+
+printtoconsole=1
+rpcallowip=::/0
+rpcpassword=${BITCOIN_RPC_PASSWORD:-password}
+rpcuser=${BITCOIN_RPC_USER:-bitcoin}
+
+# optimizations
+dbcache=100
+maxorphantx=10
+maxmempool=50
+maxconnections=40
+maxuploadtarget=5000
+
 	EOF
         chown bitcoin:bitcoin "$BITCOIN_DATA/bitcoin.conf"
     fi
